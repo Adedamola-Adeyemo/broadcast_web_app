6e3828367bb67fc6e311613b32c29151bce9f840
Adedamola Adeyemo <52447497+Adedamola-Adeyemo@users.noreply.github.com>
2020-04-20 11:56:37 +0100

    Text web app testing

---
+++ sms-env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\te\desktop\smspy\sms-env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ sms-env/web_project/broadcast/views.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.http import HttpResponse
+from twilio.rest import Client
+
+def broadcast_sms(request):
+    message_to_broadcast = ('Testing out AboCoders broadcasting web app. See what we do at https://abocoders.org.ng')
+    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+    for recipient in settings.SMS_BROADCAST_TO_NUMBERS:
+        if recipient:
+            client.messages.create(to=recipient,
+                                    from_=settings.TWILIO_NUMBER,
+                                    body=message_to_broadcast)
+    return HttpResponse('Message sent', 200)
